1de486607554ab32dd771f4526234528dbefbe94
QuantumBen <omosabenedict@gmail.com>
2023-04-14 13:11:05 +0100

    fix title and id  in Eloquentresource

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -15,7 +15,8 @@ class PostResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->title,
+            'id' => $this->id,
+            'title' => $this->title,
             'content' => substr($this->content, 0, 50) . '...',
             'created_at' => $this->created_at->toDateString()
         ];
