9f7863a2496adade469ace909c5415f1d005fbd4
QuantumBen <omosabenedict@gmail.com>
2023-04-14 10:13:34 +0100

    cleanup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
-import PostsIndex from "./components/Post.vue";
+import PostsIndex from "./components/Posts/Index.vue";
 
-const app = createApp();
-app.component('index', PostsIndex);
+const app = createApp({});
+app.component('posts-index', PostsIndex);
 app.mount('#app');
