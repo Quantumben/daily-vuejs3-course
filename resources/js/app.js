66f7f0489a2212a869db74bb6139eed63dbba2d7
QuantumBen <omosabenedict@gmail.com>
2023-04-17 04:00:18 +0100

    Add vue-route and configure it

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,19 @@
 import { createApp } from "vue";
+import { createRouter, createWebHistory } from 'vue-router';
 import PostsIndex from "./components/Posts/Index.vue";
 import { TailwindPagination } from 'laravel-vue-pagination';
 
 
+const routes = [
+    { path: '/', component: PostsIndex }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
 const app = createApp({});
-app.component('posts-index', PostsIndex);
+app.use(router);
 app.component('Pagination', TailwindPagination);
 app.mount('#app');
