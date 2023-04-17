63cda039d1ce60e90cd4279799a2b789058fab6f
QuantumBen <omosabenedict@gmail.com>
2023-04-17 04:19:59 +0100

    Use vue-router name on AppJs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,29 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from 'vue-router';
-import App from './layouts/App.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./layouts/App.vue";
 import PostsIndex from "./components/Posts/Index.vue";
 import PostsCreate from "./components/Posts/Create.vue";
-import { TailwindPagination } from 'laravel-vue-pagination';
-
+import { TailwindPagination } from "laravel-vue-pagination";
 
 const routes = [
-    { path: '/', component: PostsIndex },
-    { path: '/posts/create', component: PostsCreate },
-]
+    {
+        path: "/",
+        name: "posts.index",
+        component: PostsIndex,
+    },
+    {
+        path: "/posts/create",
+        name: "posts.create",
+        component: PostsCreate,
+    },
+];
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
-})
+    routes,
+});
 
 const app = createApp(App);
 app.use(router);
-app.component('Pagination', TailwindPagination);
-app.mount('#app');
+app.component("Pagination", TailwindPagination);
+app.mount("#app");
