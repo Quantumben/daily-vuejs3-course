a2649eaed98ece3f3446862e6e8407d4ef2f09c4
QuantumBen <omosabenedict@gmail.com>
2023-04-17 03:36:22 +0100

    Add Ordering on the table

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -1,89 +1,97 @@
 <template>
     <div class="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
         <div class="min-w-full align-middle">
-
             <div class="mb-4">
-                <select
-                    v-model="selectedCategory"
-                    class="block mt-1 w-full sm:w-1/4 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                >
+                <select v-model="selectedCategory" class="block mt-1 w-full sm:w-1/4 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                     <option value="" selected>-- Filter by category --</option>
                     <option v-for="category in categories" :key="category.id" :value="category.id">
                         {{ category.name }}
                     </option>
                 </select>
             </div>
-
-
             <table class="min-w-full divide-y divide-gray-200 border">
                 <thead>
-                    <tr>
-                        <th class="px-6 py-3 bg-gray-50 text-left">
-                            <span
-                                class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                                >ID</span
-                            >
-                        </th>
-                        <th class="px-6 py-3 bg-gray-50 text-left">
-                            <span
-                                class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                                >Title</span
-                            >
-                        </th>
-
-                        <th class="px-6 py-3 bg-gray-50 text-left">
-                            <span
-                                class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                                >Category</span
-                            >
-                        </th>
-
-                        <th class="px-6 py-3 bg-gray-50 text-left">
-                            <span
-                                class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                                >Content</span
-                            >
-                        </th>
-                        <th class="px-6 py-3 bg-gray-50 text-left">
-                            <span
-                                class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                                >Created at</span
-                            >
-                        </th>
-                    </tr>
+                <tr>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+<!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">ID</span>-->
+                        <div class="flex flex-row items-center justify-between cursor-pointer" @click="updateOrdering('id')">
+                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider" :class="{ 'font-bold text-blue-600': orderColumn === 'id' }">
+                                ID
+                            </div>
+                            <div class="select-none">
+                                <span :class="{
+                                  'text-blue-600': orderDirection === 'asc' && orderColumn === 'id',
+                                  'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'id',
+                                }">&uarr;</span>
+                                <span :class="{
+                                  'text-blue-600': orderDirection === 'desc' && orderColumn === 'id',
+                                  'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'id',
+                                }">&darr;</span>
+                            </div>
+                        </div>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+<!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</span>-->
+                        <div class="flex flex-row items-center justify-between cursor-pointer" @click="updateOrdering('title')">
+                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider" :class="{ 'font-bold text-blue-600': orderColumn === 'title' }">
+                                Title
+                            </div>
+                            <div class="select-none">
+                                <span :class="{
+                                  'text-blue-600': orderDirection === 'asc' && orderColumn === 'title',
+                                  'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'title',
+                                }">&uarr;</span>
+                                <span :class="{
+                                  'text-blue-600': orderDirection === 'desc' && orderColumn === 'title',
+                                  'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'title',
+                                }">&darr;</span>
+                            </div>
+                        </div>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Category</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+<!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Created at</span>-->
+                        <div class="flex flex-row items-center justify-between cursor-pointer" @click="updateOrdering('created_at')">
+                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider" :class="{ 'font-bold text-blue-600': orderColumn === 'created_at' }">
+                                Created at
+                            </div>
+                            <div class="select-none">
+                                <span :class="{
+                                  'text-blue-600': orderDirection === 'asc' && orderColumn === 'created_at',
+                                  'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'created_at',
+                                }">&uarr;</span>
+                                <span :class="{
+                                  'text-blue-600': orderDirection === 'desc' && orderColumn === 'created_at',
+                                  'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'created_at',
+                                }">&darr;</span>
+                            </div>
+                        </div>
+                    </th>
+                </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200 divide-solid">
-                    <tr v-for="post in posts.data" :key="post.id">
-                        <td
-                            class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"
-                        >
-                            {{ post.id }}
-                        </td>
-
-                        <td
-                            class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"
-                        >
-                            {{ post.title }}
-                        </td>
-
-                        <td
-                            class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"
-                        >
-                            {{ post.category }}
-                        </td>
-
-                        <td
-                            class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"
-                        >
-                            {{ post.content }}
-                        </td>
-
-                        <td
-                            class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"
-                        >
-                            {{ post.created_at }}
-                        </td>
-                    </tr>
+                <tr v-for="post in posts.data" :key="post.id">
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ post.id }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ post.title }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ post.category }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ post.content }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                        {{ post.created_at }}
+                    </td>
+                </tr>
                 </tbody>
             </table>
 
@@ -91,41 +99,35 @@
         </div>
     </div>
 </template>
-<!-- /*
-This code defines a Vue.js component called "Post" and uses the Vue.js composition API to fetch and display a list of posts.
 
-The import { onMounted } from 'vue' statement imports the onMounted function from the Vue.js package.
-
-The import usePosts from '../../composables/posts' statement imports a custom composable function called usePosts from the posts.js file located in the composables directory.
-
-The const { posts, getPosts } = usePosts() statement calls the usePosts() function to get the posts data and getPosts function from the posts.js file.
-
-The onMounted(getPosts) statement calls the getPosts function when the component is mounted.
-
-The return { posts } statement returns the posts data object to be used in the template of the component.
-*/ -->
 <script>
 import { ref, onMounted, watch } from "vue";
 import usePosts from "../../composables/posts";
 import useCategories from "../../composables/categories";
 
 export default {
-    name: "Post",
     setup() {
-        const selectedCategory = ref('');
-        const { posts, getPosts } = usePosts();
-        const { categories, getCategories } = useCategories();
-
+        const selectedCategory = ref('')
+        const orderColumn = ref('created_at')
+        const orderDirection = ref('desc')
+        const { posts, getPosts } = usePosts()
+        const { categories, getCategories } = useCategories()
         onMounted(() => {
-            getPosts();
-            getCategories();
-        });
+            getPosts()
+            getCategories()
+        })
+
+        const updateOrdering = (column) => {
+            orderColumn.value = column;
+            orderDirection.value = (orderDirection.value === 'asc') ? 'desc' : 'asc';
+            getPosts(1, selectedCategory.value, orderColumn.value, orderDirection.value);
+        }
 
         watch(selectedCategory, (current, previous) => {
             getPosts(1, current)
         })
 
-        return { posts, getPosts, categories, selectedCategory };
-    },
-};
+        return { posts, getPosts, categories, selectedCategory, orderColumn, orderDirection, updateOrdering }
+    }
+}
 </script>
