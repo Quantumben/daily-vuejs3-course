e72e28060ebbc62782fa53d8c5b573147077fde7
QuantumBen <omosabenedict@gmail.com>
2023-04-17 08:10:45 +0100

    Add template to Create.vue file

--- resources/js/composables/categories.js
+++ resources/js/composables/categories.js
@@ -5,10 +5,11 @@ export default function useCategories() {
     const categories = ref({});
 
     const getCategories = async() => {
-        axios.get(`api/categories`).then((response) => {
-            categories.value = response.data.data; //This is cos we are using eloquentResource
-        });
-    };
+        axios.get(`/api/categories`)
+            .then(response => {
+                categories.value = response.data.data; //This is cos we are using eloquentResource
+            })
+    }
 
     return { categories, getCategories };
 }
