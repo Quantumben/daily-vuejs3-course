473c35a1f4255ee3db45126eb679c419ff645099
QuantumBen <omosabenedict@gmail.com>
2023-04-19 06:11:09 +0100

    Add ApiResource & removing inertia default wrapper

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use inertia;
 use App\Models\Post;
+use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = PostResource::collection(Post::all());
 
         return inertia('Posts/Index', compact('posts'));
     }
