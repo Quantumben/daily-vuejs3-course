47cdf52c484fafe48dceeaff02220c80e1ce0449
QuantumBen <omosabenedict@gmail.com>
2023-04-21 20:23:04 +0100

    Add title to about page

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,4 +1,5 @@
 <template>
+     <Head title="About us" />
     <AppLayout>
         <h2 class="mt-4 text-2xl">About us</h2>
         <p>Text about us.</p>
@@ -6,11 +7,13 @@
 </template>
 <script>
 import AppLayout from "../Layouts/App.vue";
+import { Head } from "@inertiajs/vue3";
 
 export default {
     name: "About",
     components: {
         AppLayout,
+        Head
     },
 };
 </script>
