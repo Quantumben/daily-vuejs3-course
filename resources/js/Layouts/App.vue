70ee7bee3618c18cc3d8a788a761db22c8bde6bb
QuantumBen <omosabenedict@gmail.com>
2023-04-22 10:54:49 +0100

    Logout with Ziggy package

--- resources/js/Layouts/App.vue
+++ resources/js/Layouts/App.vue
@@ -47,7 +47,7 @@
                             <div>Hi, user</div>
                         </div>
                     </div>
-                    <Link href="/logout" method="post"
+                    <Link :href="route('logout')" method="post"
                         type="button"
                         class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150 ml-4"
                     >
